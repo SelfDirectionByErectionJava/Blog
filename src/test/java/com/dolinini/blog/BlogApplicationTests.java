2fcf6c65a29568deafe13055dee174d7057032ac
SelfDirectionByErectionJava <ljvghjlfnm@yandex.ru>
2020-10-17 14:15:17 +0500

    First commit

---
+++ src/test/java/com/dolinini/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dolinini.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
